5bd89f41b5e96f0d10b8a5d4467d673cb510dee5
Joel-Milligan <22701252@student.uwa.edu.au>
2020-06-08 20:23:51 +0800

    Working insertion sort function

--- main.py
+++ main.py
@@ -45,6 +45,25 @@ def run_djikstra():
     print(djikstra(source, matrix))
 
 
+def insertion_sort(array):
+    """
+    Returns a sorted version of array, using insertion sort.
+    """
+    sorted_array = array.copy()
+
+    # Start after first element, as the first element is trivially sorted.
+    for i in range(1, len(sorted_array)):
+        for j in range(i, 0, -1):
+            current_compare = sorted_array[j - 1]
+            
+            while current_compare > sorted_array[j]:
+                temp = current_compare
+                sorted_array[j - 1] = sorted_array[j]
+                sorted_array[j] = temp
+
+    return sorted_array
+
+
 def parse_matrix(matrix):
     """
     Takes an adjacency matrix representing a graph and 
